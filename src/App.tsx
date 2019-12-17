36b96ad7311895cdbd88b9143cfced94f5910a68
Theodore Park <theodore.park@kakaomobility.com>
2019-12-18 00:08:49 +0900

    react app example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
-import Profile from "./Profile";
+import Profile from "./components/Profile";
+import Counter from "./components/Counter";
+import TodoList from "./components/TodoList";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Profile job="개발자" name="테오도르" />
+        <Counter />
+        <TodoList />
       </div>
     )
   }
